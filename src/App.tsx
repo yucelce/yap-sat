68967d7e1bf387bb57512489bc9dfbb6159f62a7
yucelce <88087749+yucelce@users.noreply.github.com>
2026-06-24 09:06:44 +0300

    rvz

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Proje Başarıyla Çalışıyor!</h1>
+    </div>
+  )
+}
